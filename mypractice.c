f746d3b3e1cb154202c4cbf08c3eae655f2a6bbf
gangbenger <2608858773@qq.com>
2019-01-09 18:56:44 +0800

    thefrist

---
+++ mypractice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int mian(void)
+{
+	printf("hello world"\n);
+	
+	printf("hello tomrrow"\n);
+	
+	printf("hello myselfes"\n);
+	
+	return 0;
+	
+}
+	
